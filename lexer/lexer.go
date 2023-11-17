d699b40f771492c7255e37c1d3205db0442cb2cd
hirokis920 <hirokis920@gmail.com>
2023-11-18 08:24:13 +0900

    微修正

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -4,9 +4,9 @@ import "monkey/token"
 
 type Lexer struct {
 	input        string
-	position     int  // 入力における現在の位置（現在の文字を指し示す）
-	readPosition int  // これから読み込む位置（現在の文字の次）
-	ch           byte // 現在検査中の文字
+	position     int  // current position in input (points to current char)
+	readPosition int  // current reading position in input (after current char)
+	ch           byte // current char under examination
 }
 
 func New(input string) *Lexer {
