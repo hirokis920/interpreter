012753fc4514bb5b203d7da1509a1e9f90bfed9f
hirokis920 <hirokis920@gmail.com>
2023-11-20 06:59:59 +0900

    テスト真偽値修正

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -385,33 +385,38 @@ func testInfixExpression(t *testing.T, exp ast.Expression, left interface{}, ope
 }
 
 func TestBooleanExpression(t *testing.T) {
-	input := "true;"
-
-	l := lexer.New(input)
-	p := New(l)
-	program := p.ParseProgram()
-	checkParserErrors(t, p)
-
-	if len(program.Statements) != 1 {
-		t.Fatalf("program has not enough statements. got=%d", len(program.Statements))
+	tests := []struct {
+		input           string
+		expectedBoolean bool
+	}{
+		{"true;", true},
+		{"false;", false},
 	}
 
-	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
-	if !ok {
-		t.Fatalf("program.Statements[0] is not ast.Expression. got=%T", program.Statements[0])
-	}
+	for _, tt := range tests {
+		l := lexer.New(tt.input)
+		p := New(l)
+		program := p.ParseProgram()
+		checkParserErrors(t, p)
 
-	boo, ok := stmt.Expression.(*ast.Boolean)
-	if !ok {
-		t.Fatalf("exp not *ast.Boolean. got=%T", stmt.Expression)
-	}
+		if len(program.Statements) != 1 {
+			t.Fatalf("program has not enough statements. got=%d",
+				len(program.Statements))
+		}
 
-	if boo.Value != true {
-		t.Errorf("literal.Value not %t. got=%t", true, boo.Value)
-	}
+		stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+		if !ok {
+			t.Fatalf("program.Statements[0] is not ast.ExpressionStatement. got=%T",
+				program.Statements[0])
+		}
 
-	if boo.TokenLiteral() != "true" {
-		t.Errorf("boo.TokenLiteral not %s. got=%s", "true", boo.TokenLiteral())
+		boolean, ok := stmt.Expression.(*ast.Boolean)
+		if !ok {
+			t.Fatalf("exp not *ast.Boolean. got=%T", stmt.Expression)
+		}
+		if boolean.Value != tt.expectedBoolean {
+			t.Errorf("boolean.Value not %t. got=%t", tt.expectedBoolean,
+				boolean.Value)
+		}
 	}
-
 }
