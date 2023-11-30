a8a4444309a16591740d6ab0630839db80a8a695
hirokis920 <hirokis920@gmail.com>
2023-12-01 05:54:47 +0900

    整数リテラルの途中

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -1,1 +1,13 @@
 package evaluator
+
+import "testing"
+
+func TestEvalIntegerExpression(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected int64
+	}{
+		{"5", 5},
+		{"10", 10},
+	}
+}
