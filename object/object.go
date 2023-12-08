e2d68270c478cb7d8c2d557e8b8f7079348de9ed
hirokis920 <hirokis920@gmail.com>
2023-12-09 06:38:28 +0900

    リターンオブジェクト追加

--- object/object.go
+++ object/object.go
@@ -5,10 +5,18 @@ import "fmt"
 type ObjectType string
 
 const (
-	INTEGER_OBJ = "INTEGER"
-	NULL_OBJ    = "NULL"
-	BOOLEAN_OBJ = "BOOLEAN"
+	INTEGER_OBJ      = "INTEGER"
+	NULL_OBJ         = "NULL"
+	BOOLEAN_OBJ      = "BOOLEAN"
+	RETURN_VALUE_OBJ = "RETURN_VALUE"
 )
+
+type ReturnValue struct {
+	Value Object
+}
+
+func (rv *ReturnValue) Type() ObjectType { return RETURN_VALUE_OBJ }
+func (rv *ReturnValue) Inspect() string  { return rv.Value.Inspect() }
 
 type Object interface {
 	Type() ObjectType
