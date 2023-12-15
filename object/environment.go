824a36a416ee31fcdc48736f87780777a4fc7e11
hirokis920 <hirokis920@gmail.com>
2023-12-16 06:03:38 +0900

    環境の拡張

--- object/environment.go
+++ object/environment.go
@@ -2,6 +2,12 @@ package object
 
 //環境　letで定義する名前と関連づけられた値を記録しておくためのもの。文字列とオブジェクトを関連づけるハッシュマップ
 
+func NewEncloseEnvironment(outer *Environment) *Environment {
+	env := NewEnvironment()
+	env.outer = outer
+	return env
+}
+
 func NewEnvironment() *Environment {
 	s := make(map[string]Object)
 	return &Environment{store: s}
@@ -9,11 +15,15 @@ func NewEnvironment() *Environment {
 
 type Environment struct {
 	store map[string]Object
+	outer *Environment
 }
 
 func (e *Environment) Get(name string) (Object, bool) {
 
 	obj, ok := e.store[name]
+	if !ok && e.outer != nil {
+		obj, ok = e.outer.Get(name)
+	}
 	return obj, ok
 }
 
