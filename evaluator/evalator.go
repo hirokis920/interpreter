4fce71d1d1364d55c17a2be1409ec92323c64a37
hirokis920 <hirokis920@gmail.com>
2023-12-30 07:00:38 +0900

    ハッシュリテラルを評価する途中

--- evaluator/evalator.go
+++ evaluator/evalator.go
@@ -99,8 +99,14 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 			return index
 		}
 		return evalIndexExpression(left, index)
+	case *ast.HashLiteral:
+		return evalHashLiteral(node, env)
 	}
 	return nil
+}
+
+func evalHashLiteral(node *ast.HashLiteral, env *object.Environment) object.Object {
+	pairs := make(map[object.HashKey]object.HashPair)
 }
 
 func evalIndexExpression(left, index object.Object) object.Object {
