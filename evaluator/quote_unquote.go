d7ed2598ab34081bef1b2683a5283355e121e9e3
hirokis920 <hirokis920@gmail.com>
2024-01-07 06:36:38 +0900

    途中

--- evaluator/quote_unquote.go
+++ evaluator/quote_unquote.go
@@ -5,6 +5,34 @@ import (
 	"monkey/object"
 )
 
-func quote(node ast.Node) object.Object {
+func quote(node ast.Node, env *object.Environment) object.Object {
+	node = evalUnquoteCalls(node, env)
 	return &object.Quote{Node: node}
+}
+
+func evalUnquoteCalls(quoted ast.Node, env *object.Environment) ast.Node {
+	return ast.Modify(quoted, func(node ast.Node) ast.Node {
+		if !isUnquoteCall(node) {
+			return node
+		}
+
+		call, ok := node.(*ast.CallExpression)
+		if !ok {
+			return node
+		}
+		if len(call.Arguments) != 1 {
+			return node
+		}
+
+		return Eval(call.Arguments[0], env)
+	})
+}
+
+func isUnquoteCall(node ast.Node) bool {
+	callExpression, ok := node.(*ast.CallExpression)
+	if !ok {
+		return false
+	}
+
+	return callExpression.Function.TokenLiteral() == "unquote"
 }
